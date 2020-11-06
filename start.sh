36993924042a470efe88dbb579715986bb641be8
Treize Interactif <pierre@13i.fr>
2020-11-06 22:25:39 +0100

    Ajout du full path

--- start.sh
+++ start.sh
@@ -3,10 +3,13 @@
 # On ne lance pas conky de suite, on attend un peu pour ne pas ralentir le démarrage de l'ordinateur
 sleep 10
 
+# Dossier courrant
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # On lance chacun des composants
 COMPONENTS=(time system processes memory filesystem network mediaplayer fortune)
 
 for COMP in "${COMPONENTS[@]}"
 do
-    conky -dq -c ./components/$COMP.conf
+    conky -dq -c $DIR/components/$COMP.conf
 done
